86db4cce0c789e641327d35e78f187e97601c481
esteban-elias <esteban.elias@proton.me>
2023-08-20 22:22:35 -0400

    Add shadow to the Navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,6 +11,7 @@ const Header = () => {
       collapseOnSelect
       expand="lg"
       sticky="top"
+      className='shadow'
       style={{ backgroundColor: '#FFFFFF', padding: '0px' }}
     >
       <Container fluid>
